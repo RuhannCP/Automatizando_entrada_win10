d70889c46ec2faf85b6489f9a250d24be4a45488
RuhannCP <ruhannc@ufmg.br>
2021-10-10 17:05:14 -0300

    Automatizando com selenium e pyautogui

--- automatizador_de_inicializacao.py
+++ automatizador_de_inicializacao.py
@@ -1,128 +1,102 @@
-# To add a new cell, type ''
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 import pyperclip
 import pyautogui
+import socket
 import sys
+import time
+
+
+def internet(Host="8.8.8.8", port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((Host, port))
+        socket.setdefaulttimeout(15)
+        return True
+    except Exception:
+        return False
+
+
+# Função para chamada de um programa
+def init_program(name_program):
+    pyautogui.press('win')
+    pyperclip.copy(name_program)
+    pyautogui.hotkey('ctrl','v')
+    pyautogui.press('enter')
 
 
 if __name__=="__main__":
     name_game = None
-    option = input("O que vamos Fazer hoje ?\n (1 - Trabalhar) (2 - Estudar) (3 - Jogar)\n")
-    estilo = input("Qual o estilo de musica vamos ouvir hoje ?\n")
-
-    # Obter nome do jogo
+    option_music = None
+    estilo = None
+    
+    pyautogui.PAUSE = 2
+    
+    if internet() is False:
+        pyautogui.click(x=1135, y=745)
+        pyautogui.click(x=1198, y=185)
+        pyautogui.click(x=1242, y=273)
+        pyautogui.sleep(5) 
+        
+    # Obtendo opcoes
+    option = str(input("O que vamos Fazer hoje ?\n (1 - Trabalhar) (2 - Estudar) (3 - Jogar)\n"))
+    option_music = str(input("Deseja ouvir uma musica ?(1 - Sim)\n"))
+    
+    # Obtendo nome do Jogo
     if option == '3':
-        name_game = input('Digite o nome do jogo\n')
+        name_game = input("Digite o nome do jogo! \n")
+    
+    # Configuração para abertura do chrome 
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_argument("--start-maximized")
+    options.add_argument("user-data-dir=C:\\Users\\Ruhann.Ruhann\\AppData\\Local\\Google\\Chrome\\User Data")
+    
+    # Obtendo tipo de musica
+    if option_music == '1':
+        style = str(input(" Qual estilo de musica vc deseja ouvir?\n"))
+        
+        # Iniciando Chrome
+        w = webdriver.Chrome(options=options)
 
-
-    # Inicializando playlist
-    pyautogui.press('win')
-    pyautogui.sleep(2)
-    pyperclip.copy(r'chrome')
-    pyautogui.hotkey('ctrl','v')
-    pyautogui.press('enter')
-    pyautogui.sleep(7)
-    pyautogui.hotkey('win','up')
-    pyautogui.hotkey('win','up')
-    pyautogui.hotkey('ctrl','l')
-    pyperclip.copy(r'youtube.com')
-    pyautogui.hotkey('ctrl','v')
-    pyautogui.press('enter')
-
-    # Trabalhar
+        # Iniciando Youtube
+        w.get('https://www.youtube.com')
+        search_bar = w.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div/div[1]/input')
+        search_bar.send_keys(style)
+        time.sleep(1)
+        search_bar.send_keys(Keys.ENTER)
+        time.sleep(3)
+        video = w.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-playlist-renderer[1]/div/a/h3/span')
+        time.sleep(1)
+        video.click()
+    elif option != '3':
+        
+        # Iniciando Chrome
+        w = webdriver.Chrome(options=options)
+            
+    # Modo trabalho 
     if option == '1':
-
-        # Buscando playlist
-        pyautogui.sleep(5)
-        pyautogui.click(x=1929, y=59)
-        pyautogui.sleep(2)
-        pyperclip.copy(r'playlist para ouvir no Trabalho '+estilo)
-        pyautogui.hotkey('ctrl','v')
-        pyautogui.press('enter')
-        pyautogui.sleep(5)
-        pyautogui.click(x=1885, y=636)
-        
-        # Iniciando área de trabalho
-        pyautogui.press('win')
-        pyautogui.sleep(2)
-        pyperclip.copy(r'chrome')
-        pyautogui.hotkey('ctrl','v')
-        pyautogui.press('enter')
-        pyautogui.sleep(2)
-
-        pyautogui.press('win')
-        pyautogui.sleep(2)
-        pyperclip.copy(r'vscode')
-        pyautogui.hotkey('ctrl','v')
-        pyautogui.press('enter')
-        pyautogui.sleep(2)
-        
-        pyautogui.press('win')
-        pyautogui.sleep(2)
-        pyperclip.copy(r'telegram')
-        pyautogui.hotkey('ctrl','v')
-        pyautogui.press('enter')
-        pyautogui.sleep(2)
-
-        pyautogui.press('win')
-        pyautogui.sleep(2)
-        pyperclip.copy(r'terminal')
-        pyautogui.hotkey('ctrl','v')
-        pyautogui.press('enter')
-        pyautogui.sleep(2)
-
-    # Estudar    
+        w.execute_script('''window.open("https://duckduckgo.com/","_blank");''')
+        time.sleep(2)
+        init_program('vscode')
+        init_program('terminal')
+        init_program('telegram')
+    
+    # Modo estudo
     elif option == '2':
-
-        # Buscando playlist
-        pyautogui.sleep(5)
-        pyautogui.click(x=1929, y=59)
-        pyautogui.sleep(2)
-        pyperclip.copy('playlist para ouvir estudando '+estilo)
-        pyautogui.hotkey('ctrl','v')
-        pyautogui.press('enter')
-        pyautogui.sleep(5)
-        pyautogui.click(x=1885, y=636)
-        
-        pyautogui.press('win')
-        pyautogui.sleep(2)
-        pyperclip.copy('chrome')
-        pyautogui.hotkey('ctrl','v')
-        pyautogui.press('enter')
-        pyautogui.sleep(5)
-        pyautogui.click(x=2274, y=440)
-        
-        pyautogui.press('win')
-        pyautogui.sleep(2)
-        pyperclip.copy('bloco de notas')
-        pyautogui.hotkey('ctrl','v')
-        pyautogui.press('enter')
-        
-
-    # Jogar
+        w.execute_script('''window.open("https://duckduckgo.com/","_blank");''')
+        w.execute_script('''window.open("https://virtual.ufmg.br/minhasturmas/","_blank");''')
+        time.sleep(2)
+        init_program('bloco de notas')
+        init_program('telegram')
+    
+    # Modo jogo
     elif option == '3':
-        
-        # Buscando playlist    
-        pyautogui.sleep(5)
-        pyautogui.click(x=1929, y=59)
-        pyautogui.sleep(2)
-        pyperclip.copy(r'playlist para ouvir jogando '+estilo)
-        pyautogui.hotkey('ctrl','v')
-        pyautogui.press('enter')
-        pyautogui.sleep(5)
-        pyautogui.click(x=1885, y=636)
-        
-        pyautogui.press('win')
-        pyautogui.sleep(2)
-        pyperclip.copy(name_game)
-        pyautogui.hotkey('ctrl','v')
-        pyautogui.press('enter')
+        init_program(name_game)
 
-    # Comando usado para saber posicao do mouse 
-    # pyautogui.sleep(5)
-    # pyautogui.position()
-
-
-sys.exit()    
-
-
-
+    sys.exit()    
---
+++ readme.md
@@ -0,0 +1,95 @@
+# Carregamento de programas por perfil de uso
+    Protótipo de melhoria de experiência do usuário (XP) projetado para MS Windows 10.
+    
+    O objetivo de tal ferramenta é adicionar a possibilidade do usuário realizar tarefas específicas e focadas de acordo com um perfil de uso. Isso    
+    modifica o ambiente inicialmente multitarefa, nativo do sistema operacional, tornando-o momentaneamente monotarefa.
+
+    Tem utilidade em ambientes profissionais, onde é necessário que o usuário abra um conjunto específico de aplicações a cada inicialização. Se configurado juntamente com o modo de hibernação da máquina, possibilita o reinício mais rápido de tarefas, com o mínimo de tempo necessário para troca de contexto.
+
+## Jogue ouvindo sua música favorita
+    Um exemplo de aplicação para o usuário final é inicializar uma série de programas dada um determinada opção abrindo um playlist no Youtube.
+    (...)
+
+## Foco em homeoffice
+    Na pandemia cresceu a necessidade (...) portanto tal ferramenta pode ser vista como aliada no ganho de produtividade para usuários em início de carreira.
+
+## Configuração 
+    Para configurar bas
+
+## Bibliotecas utilizadas
+    - pyautogui
+
+
+##############################################################
+
+ 
+## Carregamento de programas por perfil de uso
+ 
+    Protótipo de melhoria de experiência do usuário (XP) projetado para MS Windows 10.
+    
+    O objetivo de tal ferramenta é adicionar a possibilidade do usuário realizar tarefas específicas e focadas de acordo com um perfil de uso. Isso    
+    modifica o ambiente inicialmente multitarefa, nativo do sistema operacional, tornando-o momentaneamente monotarefa.
+
+    Tem utilidade em ambientes profissionais, onde é necessário que o usuário abra um conjunto específico de aplicações a cada inicialização. Se configurado juntamente com o modo de hibernação da máquina, possibilita o reinício mais rápido de tarefas, com o mínimo de tempo necessário para troca de contexto.
+
+### Jogue ouvindo sua música favorita
+    Um exemplo de aplicação para o usuário final, é inicializar uma série de programas dada uma determinada opção, abrindo um playlist no Youtube e o jogo selecionado.
+
+### Foco em homeoffice
+    Nao atual cenário de pandemia, cresceu o número de pessoas que trabalham e estudam em homeoffice, portanto surgiu a necessidade de algo que trouxesse agilidade e foco no trabalho a ser executado no computador. Dessa forma, tal ferramenta pode ser vista como aliada no ganho de produtividade para usuários em sua carreira prosfissinal.
+
+ 
+## Technology 
+ 
+Tecnologias utilizadas nesse projeto:
+ 
+* Python version  3.9.2
+* pyautogui version 0.9.53
+* selenium version 3.141.0
+ 
+ 
+## Services Used
+ 
+* Github
+
+## Getting started
+ 
+* To install python:
+>    https://www.python.org/downloads/release/python-392/
+* To install pyautogui:
+>    $ pip install pyautogui
+* To install selenium:
+>    $ pip install selenium
+* To run the project:
+>    $ python automatizador_de_inicialização.py
+* Para uma melhor experiência adcione a inicialização do script ao agendador de tarefas 
+ 
+## How to use
+ 
+Here will be the images and descriptions. Principal content.
+ 
+ 
+## Features
+ 
+  - Realiza conxeção com uma rede salva.
+  - Abre 
+ 
+## Links
+ 
+  - Link of deployed application: (if has been deployed)
+  - Repository: https://link_of_repository
+    - In case of sensitive bugs like security vulnerabilities, please contact
+      directly instead of using issue tracker. We value your effort
+      to improve the security and privacy of this project!
+ 
+ 
+## Versioning
+ 
+1.0.0.0
+ 
+ 
+## Authors
+ 
+* **Ruhann Carlos Pereira de Almeida**: @RuhannCP (https://github.com/RuhannCP)
+ 
+ 
